bb06e8b188b3dedc101265fb236eb1d5bd7914cf
AW0723 <64997746+AW0723@users.noreply.github.com>
2023-04-22 18:12:34 -0700

    Attempting to merge with ECS

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,5 @@
+mod server_components;
+
 use serde::{Deserialize, Serialize};
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
@@ -7,7 +9,6 @@ use slotmap::{SlotMap, SecondaryMap, DefaultKey};
 use rapier3d::prelude::*;
 
 use shared::shared_components::*;
-mod server_components;
 use server_components::*;
 
 type Entity = DefaultKey;
@@ -172,48 +173,48 @@ impl ECS {
 //     }
 // }
 
-// #[derive(Serialize, Deserialize)]
-// struct ClientData {
-//     client_id: u8,
-//     movement: String,
-// }
+#[derive(Serialize, Deserialize)]
+struct ClientData {
+    client_id: u8,
+    movement: String,
+}
 
-// // used for any 3D value (position, velocity, acceleration)
-// struct Coords {
-//     x: f64,
-//     y: f64,
-//     z: f64,
-// }
+// used for any 3D value (position, velocity, acceleration)
+struct Coords {
+    x: f64,
+    y: f64,
+    z: f64,
+}
 
-// // TODO: merge with ClientData
-// struct Player {
-//     position: Coords,
-//     velocity: Coords,
-//     hp: u8,
-//     name: String,
-// }
+// TODO: merge with ClientData
+struct Player {
+    position: Coords,
+    velocity: Coords,
+    hp: u8,
+    name: String,
+}
 
-// // Dummy structure that holds movement just for now
-// struct GameState {
-//     players: Vec<Player>,
-//     movement: String,
-// }
+// Dummy structure that holds movement just for now
+struct GameState {
+    players: Vec<Player>,
+    movement: String,
+}
 
 fn handle_client(mut stream: TcpStream) {
     let mut client_buf = [0 as u8; 50]; // using 50 byte buf
 
     // TODO: move outside of handle client function for multiple clients
-    // let mut state = GameState {
-    //     players: Vec::new(),
-    //     movement: String::from("none")
-    // };
-    // let mut dummy_player = Player {
-    //     position: Coords {x:0.0, y:0.0, z:0.0},
-    //     velocity: Coords {x:0.0, y:0.0, z:0.0},
-    //     hp: 100,
-    //     name: String::from("Dummy McDummyFace"),
-    // };
-    // state.players.push(dummy_player);
+    let mut state = GameState {
+        players: Vec::new(),
+        movement: String::from("none")
+    };
+    let mut dummy_player = Player {
+        position: Coords {x:0.0, y:0.0, z:0.0},
+        velocity: Coords {x:0.0, y:0.0, z:0.0},
+        hp: 100,
+        name: String::from("Dummy McDummyFace"),
+    };
+    state.players.push(dummy_player);
 
     while match stream.read(&mut client_buf) {
         Ok(size) => {
