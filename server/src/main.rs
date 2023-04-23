53627925c379b10a7553b540587499be5ab79683
aberryhi <aberryhi@ucsd.edu>
2023-04-23 02:11:02 -0700

    Fixed crash (temporary fix)

--- server/src/main.rs
+++ server/src/main.rs
@@ -188,7 +188,7 @@ struct Coords {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut client_buf = [0 as u8; 50];     // using 50 byte buf
+    let mut client_buf = [0 as u8; 256];     // using 50 byte buf
     let mut coords = Coords {x:0.0, y:0.0, z:0.0};
 
     while match stream.read(&mut client_buf) {
@@ -196,17 +196,17 @@ fn handle_client(mut stream: TcpStream) {
             // process client messages
             let message : &str = str::from_utf8(&client_buf[0..size]).unwrap();
             if message.len() > 0 {
-                let value : ClientData = serde_json::from_str(message).unwrap();
+                let value : PlayerInputComponent = serde_json::from_str(message).unwrap();
 
                 // process keyboard input, update the new position of cube
-                if value.movement == "down" {
-                    coords.z += -0.1;
-                } else if value.movement == "up" {
-                    coords.z += 0.1;
-                } else if value.movement == "left" {
-                    coords.x += -0.1;
-                } else if value.movement == "right" {
-                    coords.x += 0.1;
+                if value.s_pressed {
+                    coords.z += -0.001;
+                } else if value.w_pressed {
+                    coords.z += 0.001;
+                } else if value.a_pressed {
+                    coords.x += -0.001;
+                } else if value.d_pressed {
+                    coords.x += 0.001;
                 }
 
                 // send back serialized coords to the client
@@ -214,7 +214,7 @@ fn handle_client(mut stream: TcpStream) {
                 stream.write(coords_str.as_bytes()).unwrap();
 
                 // debugging
-                println!("received movement: {}", value.movement);
+                // println!("received movement: {}", value.movement);
                 println!("sending coords: {}, {}, {}", coords.x, coords.y, coords.z);
             }
 
