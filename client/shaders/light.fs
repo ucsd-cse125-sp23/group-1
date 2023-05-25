74f94f0fb1cabb57d5f1c75044bff3738dda3f45
JacksAwOLF <chiangochen@gmail.com>
2023-05-25 11:42:43 -0700

    looks ok?

--- client/shaders/light.fs
+++ client/shaders/light.fs
@@ -2,29 +2,32 @@
 
 in vec2 TexCoords;
 in vec3 Normal;
+in vec3 TangentLightPos;
+in vec3 TangentViewPos;
+in vec3 TangentFragPos;
 in mat3 TBN;
 
 out vec4 color;
 
 uniform sampler2D texture_diffuse1;
-
 uniform int load_normal;
 uniform sampler2D texture_normal1;
 
 uniform vec3 lightAmb;
 uniform vec3 lightDif;
-uniform vec3 lightDir;
 
 void main()
 {
-    vec3 result = vec3(0.0f, 0.0f, 0.0f);
-    vec3 textureColor = texture(texture_diffuse1, TexCoords).rgb;
-
     vec3 normal = Normal;
+    vec3 lightDir = vec3(0.f, 0.f, 1.f);
+
     if (load_normal == 1) {
         vec3 tnormal = texture(texture_normal1, TexCoords).rgb;
         tnormal = normalize(tnormal * 2.0 - 1.0);
-        normal =  normalize(TBN * tnormal);
+        // normal =  normalize(TBN * tnormal);
+        normal = tnormal;
+
+        lightDir = normalize(TangentLightPos - TangentFragPos);
     }
 
     // diffuse calculation
@@ -32,7 +35,7 @@ void main()
     vec3 diffuse = diff * lightDif;
 
     // add ambient to result
-    result += (lightAmb + diffuse) * textureColor;
-
+    vec3 textureColor = texture(texture_diffuse1, TexCoords).rgb;
+    vec3 result = (lightAmb + diffuse) * textureColor;
     color = vec4(result, 1.0f);
 }
--- client/shaders/nmap.vs
+++ client/shaders/nmap.vs
@@ -23,16 +23,16 @@ void main()
     gl_Position = projection * view * model * vec4(aPos, 1.0f);
     TexCoords = aTexCoords;
 
-    vec3 T = normalize(vec3(model * vec4(aTangent,   0.0)));
-    vec3 B = normalize(vec3(model * vec4(aBitangent, 0.0)));
-    vec3 N = normalize(vec3(model * vec4(aNormal,    0.0)));
-    TBN = transpose(mat3(T, B, N));
+    mat3 normalMatrix = mat3(transpose(inverse(mat3(model))));
+    Normal = normalMatrix * aNormal;
+
+    vec3 T = normalize(vec3(model * vec4(aTangent,   1.0f)));
+    vec3 B = normalize(vec3(model * vec4(aBitangent, 1.0f)));
+    vec3 N = normalize(vec3(model * vec4(aNormal,    1.0f)));
+    TBN = mat3(T, B, N);
 
     vec3 pos = vec3(model * vec4(aPos, 1.0f));
-    TangentLightPos = TBN * (pos + lightDir);
+    TangentLightPos = TBN * (lightDir + pos);
     TangentViewPos  = TBN * viewPos;
-    TangentFragPos  = TBN * pos;
-
-    mat3 normalMatrix = transpose(inverse(mat3(model)));
-    Normal = mat3(normalMatrix) * aNormal;
+    TangentFragPos  = TBN * vec3(pos);
 }
