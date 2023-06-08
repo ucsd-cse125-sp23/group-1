21025b24e7512cc1bb35d4ba6dc12e39d8d4baa3
Jackson Conte <jacksonjconte@gmail.com>
2023-06-08 01:07:44 -0700

    stop sound fn draft

--- client/src/audio.rs
+++ client/src/audio.rs
@@ -8,7 +8,7 @@ use kira::{
 		listener::{ListenerSettings, ListenerHandle},
 		emitter::{EmitterHandle, EmitterId, EmitterSettings},
 	},
-	sound::static_sound::{StaticSoundData, StaticSoundSettings}, 
+	sound::static_sound::{StaticSoundData, StaticSoundHandle, StaticSoundSettings}, 
     tween::{Tween},
     CommandError
 };
@@ -44,13 +44,18 @@ impl VecEntry {
     }
 }
 
+/**
+ * Single audio player for the game.
+ * Ties an emitter to each sound.
+ */
 pub struct AudioPlayer {
     manager: AudioManager,
     listener: ListenerHandle,
     scene: SpatialSceneHandle,
-    sound_map: HashMap<String, StaticSoundData>,
+    source_map: HashMap<String, StaticSoundData>,
+    sound_map: HashMap<EmitterId, StaticSoundHandle>,
     e_map: HashMap<EmitterId, EmitterHandle>,
-    sound_vec: Vec<VecEntry>,
+    sound_vec: Vec<VecEntry>, // for non-looping sounds
 }
 
 impl AudioPlayer {
@@ -66,30 +71,41 @@ impl AudioPlayer {
             manager: manager,
             scene: scene,
             listener: listener,
+            source_map: HashMap::new(),
             sound_map: HashMap::new(),
             e_map: HashMap::new(),
             sound_vec: vec![],
         };
         // TODO: load from config file
-        player.sound_map.insert("fire".to_string(), StaticSoundData::from_file("resources/audio/blast0.ogg", StaticSoundSettings::default()).unwrap());
+        player.source_map.insert("fire".to_string(), StaticSoundData::from_file("resources/audio/blast0.ogg", StaticSoundSettings::default()).unwrap());
         player
     }
 
     pub fn play_sound(&mut self, name: &String, x: f32, y: f32, z: f32, source: Option<Entity>) -> Result<EmitterId,Box<dyn std::error::Error>> {
         let emitter = self.scene.add_emitter(Vector3{x, y, z}, EmitterSettings::default().persist_until_sounds_finish(true))?;
         let id = emitter.id();
-        self.manager.play(self.sound_map[name].with_settings(StaticSoundSettings::new().output_destination(&emitter)))?;
+        let sound_handle = self.manager.play(self.source_map[name].with_settings(StaticSoundSettings::new().output_destination(&emitter)))?;
         self.e_map.insert(id, emitter);
+        self.sound_map.insert(id, sound_handle);
         self.sound_vec.push(VecEntry::new(id, source),);
         // println!("{}", self.manager.num_sounds());
         Ok(id)
     }
 
-    // TODO: stop looped sound function
-    pub fn stop_sound() {
-
+    // Stops a looped sound.
+    pub fn stop_sound(&mut self, id: EmitterId) {
+        match self.sound_map.get_mut(&id) {
+            Some(handle) => {
+                handle.stop(Tween::default()).unwrap();
+                self.drop_sound(&id);
+            }
+            None => {
+                eprintln!("Sound handle not found!") // THIS SHOULD NEVER HAPPEN
+            }
+        }
     }
-    // called periodically
+
+    // Takes player position and updates listener position
     pub fn move_listener(&mut self, x: f32, y: f32, z: f32, qx: f32, qy: f32, qz: f32, qw: f32) -> Result<(),CommandError> {
         self.listener.set_position(Vector3{x:x, y:y, z:z}, 
             Tween::default()
@@ -139,8 +155,14 @@ impl AudioPlayer {
      */
     pub fn prune_emitters(&mut self) {
         while self.sound_vec.len() != 0 && self.sound_vec[0].expired() {
-            self.e_map.remove(&self.sound_vec[0].id);
+            let id = self.sound_vec[0].id;
+            self.drop_sound(&id);
             self.sound_vec.pop();
         }
+    }
+
+    fn drop_sound(&mut self, id: &EmitterId) {
+        self.e_map.remove(id);
+        self.sound_map.remove(id);
     }
 }
